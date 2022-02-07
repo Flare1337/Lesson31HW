1a9bed7893627e485cddb9bfd4decb70d7cdd9f1
Flare1337 <dimonhek1234567@mail.ru>
2022-02-07 19:52:44 +0200

    Final form))

--- src/main/java/Encryptor.java
+++ src/main/java/Encryptor.java
@@ -1,7 +1,70 @@
-public class Encryptor {
-    // NOT IMPLEMENTED YET
+import java.util.ArrayList;
 
-    public String encryptString() {
-        return "";
+public class Encryptor {
+    public static int WHITE_SPACE_ASC2_CODE = 32;
+
+    public static String encryptText(String someContent) {
+        ArrayList<String> words = new ArrayList<>();
+        StringBuilder word = new StringBuilder();
+        StringBuilder encryptedText = new StringBuilder();
+        char[] symbols = someContent.toCharArray();
+        for (int counter = 0; counter < (someContent.length() + 1); counter++) {
+            if (someContent.length() != counter && symbols[counter] != WHITE_SPACE_ASC2_CODE) {
+                word.append((symbols[counter]));
+            }
+            else {
+                if (counter != someContent.length()) {
+                    word.append(symbols[counter]);
+                }
+                words.add(encryptWord(word.toString()));
+                word = new StringBuilder();
+            }
+        }
+        for (String encryptedWord : words) {
+            encryptedText.append(encryptedWord);
+        }
+        return encryptedText.toString();
+    }
+
+    private static String encryptWord(String word) {
+        char[] chars = word.toCharArray();
+        char temp = 0;
+        char secondSymbol = chars[1];
+        char lastSymbol = chars[chars.length - 1];
+        if (lastSymbol == WHITE_SPACE_ASC2_CODE) {
+            lastSymbol = chars[chars.length - 2];
+        }
+        char[] wordInChars = replaceSymbols(chars, temp, secondSymbol, lastSymbol);
+        return convertSymbolToASC(String.valueOf(wordInChars));
+    }
+
+    private static char[] replaceSymbols(char[] chars, char temp, char secondSym, char lastSym) {
+        temp = secondSym;
+        secondSym = lastSym;
+        lastSym = temp;
+        chars[1] = secondSym;
+        if (chars[chars.length - 1] == WHITE_SPACE_ASC2_CODE) {
+            chars[chars.length - 2] = lastSym;
+        }
+        else {
+            chars[chars.length - 1] = lastSym;
+        }
+        return chars;
+    }
+
+    private static String convertSymbolToASC(String word) {
+        int[] ASC2Codes = {65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77,
+                           78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90,
+                           97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107,
+                           108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118,
+                           119, 120, 121, 122};
+        StringBuilder builder = new StringBuilder(word);
+        char[] symbols = builder.toString().toCharArray();
+        for (Integer ASCode : ASC2Codes) {
+            if (symbols[0] == ASCode) {
+                builder.replace(0,1, ASCode.toString());
+            }
+        }
+        return builder.toString();
     }
 }
--- src/test/java/EncryptorTest.java
+++ src/test/java/EncryptorTest.java
@@ -1,11 +1,13 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class EncryptorTest {
     @Test
-    public void positiveEncryptorTest() {
-    }
-
-    @Test
-    public void negativeEncryptorTest() {
+    public void encryptorTest() {
+        assertEquals("66ey" ,Encryptor.encryptText("Bye"));
+        assertEquals("72olle 109na" ,Encryptor.encryptText("Hello man"));
+        assertEquals("79en 116ow 116ereh" ,Encryptor.encryptText("One two three"));
+        assertEquals("84deyDontUnderstanh" ,Encryptor.encryptText("TheyDontUnderstand"));
     }
 }
